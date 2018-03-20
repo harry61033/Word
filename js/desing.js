dcbcab8e1b8639326486101ef4167e66643aa877
Design <harry61033@gmail.com>
2018-03-20 16:33:18 +0800

    up
    up

--- js/desing.js
+++ js/desing.js
@@ -10,7 +10,7 @@ $("#Show").click(function(){
  	$(function(){
 			$(window).scroll(function(){
 				var $scroll_top = $(window).scrollTop();
-			if($scroll_top >= 100){
+			if($scroll_top >= 500){
 				// alert();
 				$('.GraphicDesignContentContainer').css({'opacity':'1'});
 				$('.TextContent').css({'display':'block'});
@@ -27,7 +27,7 @@ $("#Show").click(function(){
  	$(function(){
  		   $(window).scroll(function(){
 				var $scroll_top = $(window).scrollTop();
-			if($scroll_top >= 400){
+			if($scroll_top >= 100){
 				// alert();
 				$('.WesitbDesignContentContainer').css({'opacity':'1'});
 				$('.TextContentTwo').css({'display':'block'});
